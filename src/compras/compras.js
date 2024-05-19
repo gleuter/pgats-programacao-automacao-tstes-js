a19421210413cb3c310e5814df8aa5d0a6be47fe
Gleuter Pereira <gleuterpereira@MacBook-Air-de-Gleuter.local>
2024-05-18 21:21:04 -0300

    Refatorando  retirando console log

--- src/compras/compras.js
+++ src/compras/compras.js
@@ -1,38 +1,31 @@
+function calcularTotal(ferramentas, comprar) {
+  let valorTotaldaComprar = 0;
+  verificaSeListaVazia(ferramentas, comprar);
 
-function calcularTotal(ferramentas,comprar){
-    let valorTotaldaComprar = 0;
-    verificaSeListaVazia(ferramentas,comprar)
-
-    for(let indexFerramentas=0; indexFerramentas <ferramentas.length; indexFerramentas++){
-        const itemLista =ferramentas[indexFerramentas];
-        // console.log(ferramentas[indexFerramentas]["nome"])
-        // console.log(ferramentas[indexFerramentas].nome)
-        for(let j=0; j<comprar.length; j++){
-            if (comprar[j] === itemLista.nome) {
-                valorTotaldaComprar += itemLista.preco
-            }
-            
-           // console.log(ferramentas[indexFerramentas].preco)
-        }
-        
+  for (
+    let indexFerramentas = 0;
+    indexFerramentas < ferramentas.length;
+    indexFerramentas++
+  ) {
+    const itemLista = ferramentas[indexFerramentas];
+       for (let j = 0; j < comprar.length; j++) {
+      if (comprar[j] === itemLista.nome) {
+        valorTotaldaComprar += itemLista.preco;
+      }
     }
+  }
 
-    if(valorTotaldaComprar === 0)
-        throw new Error("Nenhuma ferramenta desejada encontrada.")
-    // console.log(valorTotaldaComprar)
+  if (valorTotaldaComprar === 0)
+    throw new Error("Nenhuma ferramenta desejada encontrada.");
 
-
-return `O valor a pagar pelas ferramentas (UFT, TOSCA) é R$ ${valorTotaldaComprar}.00`
+  return `O valor a pagar pelas ferramentas (UFT, TOSCA) é R$ ${valorTotaldaComprar}.00`;
 }
 
-
-function verificaSeListaVazia(calcularTotal,comprar){
-    if(calcularTotal.length === 0 || comprar.length === 0)
-        throw new  Error("Ambas as listas precisam ter ao menos um item.")
-    
+function verificaSeListaVazia(calcularTotal, comprar) {
+  if (calcularTotal.length === 0 || comprar.length === 0)
+    throw new Error("Ambas as listas precisam ter ao menos um item.");
 }
-
 
 module.exports = {
-    calcularTotal
-}
+  calcularTotal,
+};
